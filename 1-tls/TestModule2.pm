45fa9fd38156cac5f490c0f6c5c80f797b5e6ec6
agent-0007 <dev@null.com>
2017-02-26 10:54:55 +0300

    test case #1

---
+++ 1-tls/TestModule2.pm
@@ -0,0 +1,22 @@
+package TestModule2;
+use strict;
+use warnings;
+
+use LWP::UserAgent;
+use HTTP::Request;
+
+
+sub connect
+{
+    my $class = shift;
+    my ($url) = @_;
+
+    my $ua = LWP::UserAgent->new();
+
+    my $request = HTTP::Request->new(GET => $url);
+    my $response = $ua->request($request);
+
+    return $response->status_line() . "\n";
+}
+
+1;
